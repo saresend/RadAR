5d6e7235a023bcf8cb0303587d57e401f36a0a15
Samuel Resendez <saresend@usc.edu>
2017-10-07 00:21:13 -0700

    round 2

--- radDB/models.py
+++ radDB/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
 
-class ARObject
+
+
+class ARObject(models.Model):
 	timeCreated = models.DateTimeField(auto_now_add=True)
 	owner = models.TextField()
 	description = models.TextField()
