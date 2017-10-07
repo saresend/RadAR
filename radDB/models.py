0dd9aca0fdd079f58d9b2dd3ce1b890aed1e8450
suvir-c <scoppara@usc.edu>
2017-10-07 00:12:52 -0700

    initialized AR object model in radDB

--- radDB/models.py
+++ radDB/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
-# Create your models here.
+class ARObject
+	timeCreated = models.DateTimeField(auto_now_add=True)
+	owner = models.TextField()
+	description = models.TextField()
+	location = models.PointField()
+	asset = models.FileField(upload_to='media')		
+	
+	class Meta:
+		ordering = ('timeCreated')
